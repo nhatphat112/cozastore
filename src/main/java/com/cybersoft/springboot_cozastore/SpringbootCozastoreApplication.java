95bf47289fd439915d8827ce382659ca0476ca66
nhatphat112 <127173620+nhatphat112@users.noreply.github.com>
2023-06-15 07:34:03 +0700

    create project

---
+++ src/main/java/com/cybersoft/springboot_cozastore/SpringbootCozastoreApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.springboot_cozastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCozastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCozastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/springboot_cozastore/SpringbootCozastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.springboot_cozastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCozastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
